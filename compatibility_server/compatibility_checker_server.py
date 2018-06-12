717231403d9b32564e285ef44a69dee96988b44e
Brian Quinlan <brian@sweetapp.com>
2018-06-11 17:04:56 -0700

    Update copyright header.

--- compatibility_server/compatibility_checker_server.py
+++ compatibility_server/compatibility_checker_server.py
@@ -1,3 +1,25 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# An image used to run a Python webserver that does compatibility checking
+# between pip-installable packages.
+#
+# Contains three indenpendent versions of Python:
+#  - python 3.6: used to run the web application
+#  - python 3.6.5: used for python3 pip installation (build from source)
+#  - python 2.7.15: used for python2 pip installation (build from source)
+
 """A HTTP server that wraps pip_checker."""
 
 import argparse
