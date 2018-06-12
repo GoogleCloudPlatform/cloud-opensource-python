b034e8c04a8e25e2aa7c9682822da34a107b1d9e
Angela Li <yanhuil@google.com>
2018-06-11 17:23:36 -0700

    Add nox config for lint and test

---
+++ nox.py
@@ -0,0 +1,47 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import nox
+
+LINT_UNIT_DIR = ['compatibility_server',]
+
+
+@nox.session
+def lint(session):
+    """Run flake8.
+    Returns a failure if flake8 finds linting errors or sufficiently
+    serious code quality issues.
+    """
+    session.interpreter = 'python3.6'
+    session.install('flake8')
+    session.run('flake8',
+                ','.join(LINT_UNIT_DIR),
+                '--exclude=test_*')
+
+
+@nox.session
+@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
+def unit(session, py):
+    """Run the unit test suite."""
+
+    # Run unit tests against all supported versions of Python.
+    session.interpreter = 'python{}'.format(py)
+
+    # Run py.test against the unit tests.
+    session.run(
+        'py.test',
+        '--quiet',
+        ','.join(LINT_UNIT_DIR),
+        *session.posargs
+    )
