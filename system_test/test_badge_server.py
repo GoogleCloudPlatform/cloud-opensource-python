e4d0d8c91157cae55a911b9ce16a21792913bb8f
Angela Li <yanhuil@google.com>
2018-08-16 15:49:32 -0700

    Add tests

--- system_test/test_badge_server.py
+++ system_test/test_badge_server.py
@@ -29,6 +29,30 @@ PACKAGE_FOR_TEST = 'opencensus'
 RETRY_WAIT_PERIOD = 8000 # Wait 8 seconds between each retry
 RETRY_MAX_ATTEMPT = 10 # Retry 10 times
 
+EXPECTED_SVG = b'<svg xmlns="http://www.w3.org/2000/svg" xmlns:' \
+                           b'xlink="http://www.w3.org/1999/xlink" ' \
+                           b'width="164" height="20"><linearGradient id="b" ' \
+                           b'x2="0" y2="100%"><stop offset="0" ' \
+                           b'stop-color="#bbb" stop-opacity=".1"/><stop ' \
+                           b'offset="1" stop-opacity=".1"/></linearGradient>' \
+                           b'<clipPath id="a"><rect width="164" height="20" ' \
+                           b'rx="3" fill="#fff"/></clipPath><g clip-path=' \
+                           b'"url(#a)"><path fill="#555" d="M0 0h75v20H0z"/>' \
+                           b'<path fill="#007ec6" d="M75 0h89v20H75z"/>' \
+                           b'<path fill="url(#b)" d="M0 0h164v20H0z"/>' \
+                           b'</g><g fill="#fff" text-anchor="middle" font-' \
+                           b'family="DejaVu Sans,Verdana,Geneva,sans-serif" ' \
+                           b'font-size="110"> <text x="385" y="150" ' \
+                           b'fill="#010101" fill-opacity=".3" transform=' \
+                           b'"scale(.1)" textLength="650">opencensus</text>' \
+                           b'<text x="385" y="140" transform="scale(.1)" ' \
+                           b'textLength="650">opencensus</text><text ' \
+                           b'x="1185" y="150" fill="#010101" fill-opacity=' \
+                           b'".3" transform="scale(.1)" textLength="790">' \
+                           b'CALCULATING</text><text x="1185" y="140" ' \
+                           b'transform="scale(.1)" textLength="790">' \
+                           b'CALCULATING</text></g> </svg>'
+
 
 def wait_app_to_start():
     """Wait the application to start running."""
@@ -72,29 +96,18 @@ class TestBadgeServer(unittest.TestCase):
                 BASE_URL, PACKAGE_FOR_TEST))
         status_code = response.status_code
         content = response.content
-        expected_content = b'<svg xmlns="http://www.w3.org/2000/svg" xmlns:' \
-                           b'xlink="http://www.w3.org/1999/xlink" ' \
-                           b'width="164" height="20"><linearGradient id="b" ' \
-                           b'x2="0" y2="100%"><stop offset="0" ' \
-                           b'stop-color="#bbb" stop-opacity=".1"/><stop ' \
-                           b'offset="1" stop-opacity=".1"/></linearGradient>' \
-                           b'<clipPath id="a"><rect width="164" height="20" ' \
-                           b'rx="3" fill="#fff"/></clipPath><g clip-path=' \
-                           b'"url(#a)"><path fill="#555" d="M0 0h75v20H0z"/>' \
-                           b'<path fill="#007ec6" d="M75 0h89v20H75z"/>' \
-                           b'<path fill="url(#b)" d="M0 0h164v20H0z"/>' \
-                           b'</g><g fill="#fff" text-anchor="middle" font-' \
-                           b'family="DejaVu Sans,Verdana,Geneva,sans-serif" ' \
-                           b'font-size="110"> <text x="385" y="150" ' \
-                           b'fill="#010101" fill-opacity=".3" transform=' \
-                           b'"scale(.1)" textLength="650">opencensus</text>' \
-                           b'<text x="385" y="140" transform="scale(.1)" ' \
-                           b'textLength="650">opencensus</text><text ' \
-                           b'x="1185" y="150" fill="#010101" fill-opacity=' \
-                           b'".3" transform="scale(.1)" textLength="790">' \
-                           b'CALCULATING</text><text x="1185" y="140" ' \
-                           b'transform="scale(.1)" textLength="790">' \
-                           b'CALCULATING</text></g> </svg>'
 
         self.assertEqual(status_code, 200)
-        self.assertEqual(content, expected_content)
+        self.assertEqual(content, EXPECTED_SVG)
+
+    @retry(wait_fixed=RETRY_WAIT_PERIOD,
+           stop_max_attempt_number=RETRY_MAX_ATTEMPT)
+    def test_google_compatibility_badge(self):
+        response = requests.get(
+            '{}google_compatibility_badge/image?package={}'.format(
+                BASE_URL, PACKAGE_FOR_TEST))
+        status_code = response.status_code
+        content = response.content
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(content, EXPECTED_SVG)
