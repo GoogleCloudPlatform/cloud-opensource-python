67b9f5cb662d35581f7112cd688eda5b59c58416
Angela Li <yanhuil@google.com>
2018-09-13 16:24:02 -0700

    Add main page for dashboard (#99)

---
+++ dashboard/js/main.js
@@ -0,0 +1,93 @@
+
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  try {
+    // Percent Chart
+    var ctx = document.getElementById("percent-chart");
+    if (ctx) {
+      ctx.height = 280;
+      var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              label: "Compatibility Status",
+              data: [31, 8],
+              backgroundColor: [
+                '#00b5e9',
+                '#fa4251'
+              ],
+              hoverBackgroundColor: [
+                '#00b5e9',
+                '#fa4251'
+              ],
+              borderWidth: [
+                0, 0
+              ],
+              hoverBorderColor: [
+                'transparent',
+                'transparent'
+              ]
+            }
+          ],
+          labels: [
+            'Success',
+            'Having Conflicts'
+          ]
+        },
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          cutoutPercentage: 55,
+          animation: {
+            animateScale: true,
+            animateRotate: true
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            titleFontFamily: "Poppins",
+            xPadding: 15,
+            yPadding: 10,
+            caretPadding: 0,
+            bodyFontSize: 16
+          }
+        }
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+})(jQuery);
+
+(function ($) {
+    // USE STRICT
+    "use strict";
+    $(".animsition").animsition({
+      inClass: 'fade-in',
+      outClass: 'fade-out',
+      inDuration: 900,
+      outDuration: 900,
+      linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])',
+      loading: true,
+      loadingParentElement: 'html',
+      loadingClass: 'page-loader',
+      loadingInner: '<div class="page-loader__spin"></div>',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: ['animation-duration', '-webkit-animation-duration'],
+      overlay: false,
+      overlayClass: 'animsition-overlay-slide',
+      overlayParentElement: 'html',
+      transition: function (url) {
+        window.location.href = url;
+      }
+    });
+
+})(jQuery);
