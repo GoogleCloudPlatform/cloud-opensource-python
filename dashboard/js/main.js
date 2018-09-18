302f326a3821e5c917fdff65643019cf2f76b375
Angela Li <yanhuil@google.com>
2018-09-18 13:59:29 -0700

    Pass data using html data attribute

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,4 +1,60 @@
 (function ($) {
+    var doughnutChart = document.getElementById("percent-chart");
+    var num_success = doughnutChart.getAttribute("data-success");
+    var num_conflict = doughnutChart.getAttribute("data-conflict");
+    doughnutChart.height = 280;
+    new Chart(doughnutChart, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              label: "Compatibility Status",
+              data: [num_success, num_conflict],
+              backgroundColor: [
+                '#00b5e9',
+                '#fa4251'
+              ],
+              hoverBackgroundColor: [
+                '#00b5e9',
+                '#fa4251'
+              ],
+              borderWidth: [
+                0, 0
+              ],
+              hoverBorderColor: [
+                'transparent',
+                'transparent'
+              ]
+            }
+          ],
+          labels: [
+            'Success',
+            'Having Conflicts'
+          ]
+        },
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          cutoutPercentage: 55,
+          animation: {
+            animateScale: true,
+            animateRotate: true
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            titleFontFamily: "Poppins",
+            xPadding: 15,
+            yPadding: 10,
+            caretPadding: 0,
+            bodyFontSize: 16
+          }
+        }
+    });
+})(jQuery);
+
+(function ($) {
     // USE STRICT
     "use strict";
     $(".animsition").animsition({
