db4141b6e29af8b45b829ffd58a2e0d3b6efb751
Angela Li <yanhuil@google.com>
2018-09-20 17:43:01 -0700

    Make the tables display the real data

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -135,6 +135,29 @@
 
 })(jQuery);
 
+// Sort the table rows by the details column to display the packages having
+// issues first.
+(function ($) {
+    var table = document.getElementById("package-details");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[3];
+          y = rows[i + 1].getElementsByTagName("TD")[3];
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+    }
+})(jQuery);
 
 (function ($) {
     // USE STRICT
