2543612d95e950981d47623cc481d0ada02b8987
Angela Li <yanhuil@google.com>
2018-10-16 13:41:55 -0700

    test skeleton

---
+++ badge_server/test_badge_server.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+import unittest
+
+import badge_server
+
+
+class TestBadgeServer(unittest.TestCase):
+
+    def test__get_pair_status_for_packages(self):
+        pass
+
+    def test__sanitize_package_name(self):
+        pass
+
+    def test__get_badge_url(self):
+        pass
+
+    def test__get_self_compatibility_from_cache(self):
+        pass
+
+    def test__get_google_compatibility_from_cache(self):
+        pass
+
+    def test__get_dependency_result_from_cache(self):
+        pass
+
+    def test__get_all_results_from_cache(self):
+        pass
