a681da239bf87b5aedec58b64a3e8eca8cd5a1e6
Lily Li <pekopeko@google.com>
2019-01-29 17:25:47 -0800

    use filler names for view names, add metric removal

--- compatibility_server/custom_metric_test.py
+++ compatibility_server/custom_metric_test.py
@@ -1,7 +1,31 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Serves as an example of how to create and record custom metrics.
+
+Note: This script relies on having authentication via Google Cloud SDK
+See https://googleapis.github.io/google-cloud-python/latest/core/auth.html
+
+After running this script, the metrics can be viewed on stackdriver
+See https://app.google.stackdriver.com/metrics-explorer?project=python-compatibility-tools
+"""
+
 import random
 import time
 from pprint import pprint
 
+from google.cloud import monitoring_v3
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
@@ -12,40 +36,40 @@ from opencensus.tags import tag_map as tag_map_module
 # create metrics
 PROJECT_ID = 'python-compatibility-tools'
 
-DOCKER_ERROR_MEASURE = measure_module.MeasureInt(
-    'docker_error', 'The number of docker errors.', 'Errors')
+ERROR_MEASURE = measure_module.MeasureInt(
+    'custom_test_error', 'The number of errors.', 'Errors')
 
 # count aggregation
 COUNT_VIEW = view_module.View(
-    "docker_error_count",
-    "The number of the docker errors",
+    "testing_count_aggregation",
+    "The number of errors",
     [],
-    DOCKER_ERROR_MEASURE,
+    ERROR_MEASURE,
     aggregation_module.CountAggregation())
 
 # distribution aggregation
 # DISTRIBUTION_VIEW = view_module.View(
-#     "docker_error_distribution",
-#     "The distribution of the docker errors",
+#     "testing_distribution_aggregation",
+#     "The distribution of errors",
 #     [],
-#     DOCKER_ERROR_MEASURE,
+#     ERROR_MEASURE,
 #     aggregation_module.DistributionAggregation(
 #         [0.0, 100.0]))
 
 # LastValue Aggregation
 # LAST_VALUE_VIEW = view_module.View(
-#     "docker_error_last_value",
-#     "The last value of the docker errors",
+#     "testing_last_value_aggregation",
+#     "The last value of reported errors",
 #     [],
-#     DOCKER_ERROR_MEASURE,
+#     ERROR_MEASURE,
 #     aggregation_module.LastValueAggregation())
 
 # sum aggregation
 # SUM_VIEW = view_module.View(
-#     "docker_error_sum",
-#     "The total number of docker errors",
+#     "testing_sum_aggregation",
+#     "The total number of errors",
 #     [],
-#     DOCKER_ERROR_MEASURE,
+#     ERROR_MEASURE,
 #     aggregation_module.SumAggregation())
 
 def _enable_metrics(stats, view, export_to_stackdriver=True):
@@ -56,6 +80,11 @@ def _enable_metrics(stats, view, export_to_stackdriver=True):
         view_manager.register_exporter(exporter)
     view_manager.register_view(view)
 
+def _remove_from_stackdriver(project_id, view_name):
+    client = monitoring_v3.MetricServiceClient()
+    metric_url = 'custom.googleapis.com/opencensus/%s' % view_name
+    name = 'projects/%s/metricDescriptors/%s' % (project_id, metric_url)
+    client.delete_metric_descriptor(name)
 
 stats = stats_module.Stats()
 _enable_metrics(stats, COUNT_VIEW)
@@ -75,20 +104,30 @@ TMAP = tag_map_module.TagMap()
 results = [random.randint(0, 1) for _ in range(100)]
 for res in results:
     if res == 1:
-        COUNT_MMAP.measure_int_put(DOCKER_ERROR_MEASURE, 1)
+        COUNT_MMAP.measure_int_put(ERROR_MEASURE, 1)
         COUNT_MMAP.record(TMAP)
         # DISTRIBUTION_MMAP.record(TMAP)
         # LAST_VALUE_MMAP.record(TMAP)
         # SUM_MMAP.record(TMAP)
 
-view_names = ['docker_error_count',
-              # 'docker_error_distribution',
-              # 'docker_error_last_value',
-              # 'docker_error_sum'
+view_names = ['testing_count_aggregation',
+              # 'testing_distribution_aggregation',
+              # 'testing_last_value_aggregation',
+              # 'testing_sum_aggregation'
              ]
+
+# Print the metrics
 for view_name in view_names:
     view_data = stats.view_manager.get_view(view_name)
     pprint(vars(view_data))
     for k, v in view_data._tag_value_aggregation_data_map.items():
         pprint(k)
         pprint(vars(v))
+
+# Need extra time to create the metric in order for it to be deleted without
+# exceptions
+time.sleep(300)
+
+# Remove metrics from stackdriver
+for view_name in view_names:
+    _remove_from_stackdriver(PROJECT_ID, view_name)
