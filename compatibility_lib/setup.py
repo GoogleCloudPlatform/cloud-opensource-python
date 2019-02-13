24594d3ba7ea6e8a4f9de372ce5ec9af870e8bfa
Angela Li <yanhuil@google.com>
2019-02-13 11:28:56 -0800

    Prepare for 0.0.20 release (#244)

--- compatibility_lib/setup.py
+++ compatibility_lib/setup.py
@@ -23,7 +23,7 @@ namespaces = ['compatibility_lib']
 
 setuptools.setup(
     name="compatibility_lib",
-    version="0.0.19",
+    version="0.0.20",
     author="Cloud Python",
     description="A library to get and store the dependency compatibility "
                 "status data to BigQuery.",
