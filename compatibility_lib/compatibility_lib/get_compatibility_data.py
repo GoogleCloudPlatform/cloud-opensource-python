1d8d24bd60ca415cd5450fb05d09cf721e75581b
Brian Quinlan <brian@sweetapp.com>
2019-07-09 10:58:52 -0700

    Update maximum workers to 400
    This matches the current production compatibility server configuration.

--- compatibility_lib/compatibility_lib/get_compatibility_data.py
+++ compatibility_lib/compatibility_lib/get_compatibility_data.py
@@ -29,7 +29,9 @@ from compatibility_lib import compatibility_store
 from compatibility_lib import configs
 from compatibility_lib import package
 
-checker = compatibility_checker.CompatibilityChecker(max_workers=800)
+# The production compatibility server configuration currently runs
+# 200 pods and each pod has two workers.  
+checker = compatibility_checker.CompatibilityChecker(max_workers=400)
 store = compatibility_store.CompatibilityStore()
 
 PY2 = '2'
